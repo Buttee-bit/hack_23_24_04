2d55a41699f04dcbdcc326881f1a10e2670e6e15
DadyshkaGoga <95309197+DadyshkaGoga@users.noreply.github.com>
2024-03-23 11:49:10 +0300

    [back ?]

---
+++ backend/datebase.py
@@ -0,0 +1,25 @@
+from typing import AsyncGenerator
+import logging
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+from .config import DB_HOST, DB_PORT, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD
+
+
+DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{DB_HOST}:{DB_PORT}/{POSTGRES_DB}"
+Base = declarative_base()
+
+
+logging.warning(DATABASE_URL)
+
+
+engine = create_async_engine(DATABASE_URL)
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        logging.warning("Соединение с БД")
+        yield session
+        
