71e3f67dacce48108f4685859cce8694edf52b22
unhaten <csgold2002@mail.ru>
2024-03-23 12:59:28 +0300

    placement of block in map

--- client/src/pages/map/MapPage.tsx
+++ client/src/pages/map/MapPage.tsx
@@ -1,5 +1,12 @@
 const MapPage = () => {
-	return <main>This is a test</main>
+	return (
+		<main className='h-screen'>
+			<div className='flex gap-10 justify-between h-full'>
+				<div className='bg-red-300 w-1/6 h-full'></div>
+				<div className='h-96 w-full bg-cyan-300'></div>
+			</div>
+		</main>
+	)
 }
 
 export default MapPage
