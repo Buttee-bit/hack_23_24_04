7571dc67244b13943dfce2d62ee25dc605df3a07
DadyshkaGoga <95309197+DadyshkaGoga@users.noreply.github.com>
2024-03-23 14:03:10 +0300

    [docker]

--- backend/src/insert_data/helper.py
+++ backend/src/insert_data/helper.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 def load_data():
-    data = pd.read_csv(r'backend\src\insert_data\poi.csv', delimiter='|') #r'backend\src\insert_data\poi.csv'
+    data = pd.read_csv(r'src/insert_data/poi.csv', delimiter='|') #r'backend\src\insert_data\poi.csv'
     return data
 
 def load_data_reality():
-    data = pd.read_csv(r'backend\src\insert_data\realty.csv', delimiter=',')
-    return data.head()
+    data = pd.read_csv(r'src/insert_data/realty.csv', delimiter=',')
+    return data
 
 
 
