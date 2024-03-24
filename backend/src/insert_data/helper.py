6ec669107ba100243f5cd7be56b5f26ec04b8906
DadyshkaGoga <95309197+DadyshkaGoga@users.noreply.github.com>
2024-03-24 14:05:05 +0300

    [del trash]

--- backend/src/insert_data/helper.py
+++ backend/src/insert_data/helper.py
@@ -3,23 +3,23 @@ import json
 import csv
 import re
 from ..map.geo_market.db import get_sync_session
-from sqlalchemy import and_,delete, desc, func, insert, select, update
+from sqlalchemy import insert
+
 from ..models import Tourist_attractions
+
 def load_data():
-    data = pd.read_csv(r'src/insert_data/poi.csv', delimiter='|') #r'backend\src\insert_data\poi.csv'
+    data = pd.read_csv(r'src/insert_data/poi.csv', delimiter='|')
     return data
 
 def load_data_reality():
     data = pd.read_csv(r'src/insert_data/realty.csv', delimiter=',')
     return data
 
-
 def get_data_json():
     with open(r'src/insert_data/Russian_Underground.json', encoding='utf-8') as file:
         data = json.load(file)
         data = data[1]
         return data
-    
 
 def extract_coordinates(geolocation_string):
     matches = re.findall(r"[-+]?\d*\.\d+|\d+", geolocation_string)
@@ -34,12 +34,12 @@ def get_data_csv():
     session = get_sync_session()
     with open(r'src/insert_data/tourist_attractions.csv', newline='', encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile)
-        next(reader)  # Пропускаем заголовок
+        next(reader)
         for row in reader:
-            name = row[0].strip("()").strip("'")  # Удаляем скобки и кавычки из имени
-            type_ = row[1].strip("'")  # Удаляем кавычки из типа
-            region = row[2].strip("'")  # Удаляем кавычки из региона
-            locality = row[3].strip("'")  # Удаляем кавычки из населенного пункта
+            name = row[0].strip("()").strip("'") 
+            type_ = row[1].strip("'")
+            region = row[2].strip("'")
+            locality = row[3].strip("'")
             geolocation = row[4]
             
             if "St. Petersburg" in locality:
@@ -55,7 +55,3 @@ def get_data_csv():
                 )
                 session.execute(stmt)
                 session.commit()
-
-# get_data_json()
-# data = load_data()
-# process_data(data)
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
-from .test.router import router as test_router
 from .insert_data.router import router as insert_router
 from .map.router import router as custom_router
 
@@ -30,6 +29,5 @@ app.add_middleware(
                    "Access-Control-Allow-Origin", "Authorization"],
 )
 
-app.include_router(test_router)
 app.include_router(insert_router)
 app.include_router(custom_router)
